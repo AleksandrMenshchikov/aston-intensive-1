28259162b690b397472096e863792414f9cb7283
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2025-02-14 20:01:55 +0300

    Исправить customBind

--- lesson-4/customBind.js
+++ lesson-4/customBind.js
@@ -1,7 +1,18 @@
-Function.prototype.customBind = function (obj, ...args) {
+Function.prototype.customBind = function (obj) {
     let func = this;
 
-    return function () {
-        func.apply(obj, [...args]);
+    return function (...args) {
+        const uniqueKey = Date.now();
+        obj[uniqueKey] = func;
+        obj[uniqueKey](...args);
     };
 };
+
+const someThis = {x: 6}
+
+function foo(someArg) {
+    console.log(this.x + someArg)
+}
+
+foo.customBind(someThis)(15)
+foo.bind(someThis)(15)
